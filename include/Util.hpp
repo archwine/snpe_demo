7a30f22e8741f8b857a45d3166fc62582b5c21c4
archwine <7epimenides@gmail.com>
2023-11-22 18:05:06 +0800

    feat: update Util

--- include/Util.hpp
+++ include/Util.hpp
@@ -1,11 +1,3 @@
-//==============================================================================
-//
-//  Copyright (c) 2017-2021 Qualcomm Technologies, Inc.
-//  All Rights Reserved.
-//  Confidential and Proprietary - Qualcomm Technologies, Inc.
-//
-//==============================================================================
-
 #ifndef UTIL_H
 #define UTIL_H
 
@@ -16,41 +8,67 @@
 #include "DlSystem/ITensorFactory.hpp"
 #include "DlSystem/TensorShape.hpp"
 
+// 将一个字符串根据指定的分割符, 分割成一个容器
 template <typename Container> Container& split(Container& result, const typename Container::value_type & s, typename Container::value_type::value_type delimiter )
 {
+  // 清理结果容器 result
   result.clear();
+  // 用字符串s 构造一个输入字符串流 ss
   std::istringstream ss( s );
+  // 循环读取 ss 知道结束
   while (!ss.eof())
   {
     typename Container::value_type field;
+    // getline可以根据分隔符拆分字符串
     getline( ss, field, delimiter );
+    // 判断空字段并跳过
     if (field.empty()) continue;
     result.push_back( field );
   }
   return result;
 }
 
+// 计算张量的大小, dims 指向维度数组的指针, 张量的维度, 张量中每个元素的大小
 size_t calcSizeFromDims(const zdl::DlSystem::Dimension *dims, size_t rank, size_t elementSize);
 
+// 从文件中加载浮点数据
 std::vector<float> loadFloatDataFile(const std::string& inputFile);
-std::vector<unsigned char> loadByteDataFile(const std::string& inputFile);
-template<typename T> bool loadByteDataFile(const std::string& inputFile, std::vector<T>& loadVector);
 
+// 从文件中加载字节数据
+std::vector<unsigned char> loadByteDataFile(const std::string& inputFile);
+
+// 从文件中加载字节数据到 std::vector 容器中. 该函数接受两个参数, inputFile: 输入文件路径, loadVector: std::vector 容器，用于存储字节数据
+template<typename T> 
+bool loadByteDataFile(const std::string& inputFile, std::vector<T>& loadVector);
+
+// 从文件中以批量方式加载字节数据
 std::vector<unsigned char> loadByteDataFileBatched(const std::string& inputFile);
-template<typename T> bool loadByteDataFileBatched(const std::string& inputFile, std::vector<T>& loadVector, size_t offset);
+
+// 上边的函数loadByteDataFileBatched会调用, 将 loadVector 作为返回值返回 
+template<typename T> 
+bool loadByteDataFileBatched(const std::string& inputFile, std::vector<T>& loadVector, size_t offset);
+
+// 从文件中加载TF8格式的数据
 bool loadByteDataFileBatchedTf8(const std::string& inputFile, std::vector<uint8_t>& loadVector, size_t offset);
+// 从文件中加载TF-N格式的数据
 bool loadByteDataFileBatchedTfN(const std::string& inputFile, std::vector<uint8_t>& loadVector, size_t offset,
                                 unsigned char& stepEquivalentTo0, float& quantizedStepSize, bool staticQuantization, int bitWidth);
 
+// 将 ITensor 分批保存到文件中
 bool SaveITensorBatched(const std::string& path, const zdl::DlSystem::ITensor* tensor, size_t batchIndex=0, size_t batchChunk=0);
+// 将用户缓冲区的内容分批保存到文件中
 bool SaveUserBufferBatched(const std::string& path, const std::vector<uint8_t>& buffer, size_t batchIndex=0, size_t batchChunk=0);
+// 确保指定的目录存在
 bool EnsureDirectory(const std::string& dir);
 
+// 将 uint8_t 或者 uint16_t 类型的量化数据转换为浮点数据
 void TfNToFloat(float *out, uint8_t *in, const unsigned char stepEquivalentTo0, const float quantizedStepSize, size_t numElement, int bitWidth);
+// 浮点数数组转换为量化数组
 bool FloatToTfN(uint8_t* out, unsigned char& stepEquivalentTo0, float& quantizedStepSize, bool staticQuantization, float* in, size_t numElement, int bitWidth);
 
+// 设置可调整的维度
 void setResizableDim(size_t resizableDim);
+// 返回 resizable_dim 变量的值
 size_t getResizableDim();
 
 #endif
-
--- src/Util.cpp
+++ src/Util.cpp
@@ -1,11 +1,3 @@
-//==============================================================================
-//
-//  Copyright (c) 2017-2021,2023 Qualcomm Technologies, Inc.
-//  All Rights Reserved.
-//  Confidential and Proprietary - Qualcomm Technologies, Inc.
-//
-//==============================================================================
-
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -36,22 +28,24 @@ size_t calcSizeFromDims(const zdl::DlSystem::Dimension *dims, size_t rank, size_
    return size;
 }
 
-
 bool EnsureDirectory(const std::string& dir)
 {
    auto i = dir.find_last_of('/');
    std::string prefix = dir.substr(0, i);
 
+   // 检查路径是否为空、"." 或 ".."。如果是，则直接返回 true。
    if (dir.empty() || dir == "." || dir == "..")
    {
       return true;
    }
 
+   // 检查路径是否存在, 如果不存在, 则递归调用 EnsureDirectory() 函数它
    if (i != std::string::npos && !EnsureDirectory(prefix))
    {
       return false;
    }
 
+   // 尝试创建目录. 如果成功, 则返回 true
    int rc = mkdir(dir.c_str(),  S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
    if (rc == -1 && errno != EEXIST)
    {
@@ -83,24 +77,29 @@ std::vector<unsigned char> loadByteDataFile(const std::string& inputFile)
    return vec;
 }
 
+// 将 inputFile 里的数据加载到 loadVector 变量里头
 template<typename T>
 bool loadByteDataFile(const std::string& inputFile, std::vector<T>& loadVector)
 {
+   // 打开输入文件, 检查是否可以被打开, 如果无法被打开, 函数会打印错误信息并返回 false
    std::ifstream in(inputFile, std::ifstream::binary);
    if (!in.is_open() || !in.good())
    {
       std::cerr << "Failed to open input file: " << inputFile << "\n";
    }
 
+   // 获取输入文件的长度
    in.seekg(0, in.end);
    size_t length = in.tellg();
    in.seekg(0, in.beg);
 
+   // 检查输入文件的长度是否是数据类型 T 的倍数。如果不是，该函数会打印错误信息并返回 false
    if (length % sizeof(T) != 0) {
       std::cerr << "Size of input file should be divisible by sizeof(dtype).\n";
       return false;
    }
 
+   // 调整 loadVector 容器的大小，使其与输入文件的长度相同
    if (loadVector.size() == 0) {
       loadVector.resize(length / sizeof(T));
    } else if (loadVector.size() < length / sizeof(T)) {
@@ -108,6 +107,7 @@ bool loadByteDataFile(const std::string& inputFile, std::vector<T>& loadVector)
       loadVector.resize(length / sizeof(T));
    }
 
+   // 将输入文件中的字节数据读取到 loadVector 容器中
    if (!in.read(reinterpret_cast<char*>(&loadVector[0]), length))
    {
       std::cerr << "Failed to read the contents of: " << inputFile << "\n";
@@ -123,6 +123,7 @@ std::vector<unsigned char> loadByteDataFileBatched(const std::string& inputFile)
    return vec;
 }
 
+// 从文件中批量加载字节数据到 loadVector 容器中
 template<typename T>
 bool loadByteDataFileBatched(const std::string& inputFile, std::vector<T>& loadVector, size_t offset)
 {
@@ -156,6 +157,14 @@ bool loadByteDataFileBatched(const std::string& inputFile, std::vector<T>& loadV
     return true;
 }
 
+/*
+    out 指向存储转换后浮点数据的数组
+    in 指向存储量化数据的数组
+    stepEquivalentTo0 量化步长为 0 的值
+    quantizedStepSize: 量化步长
+    numElement: 量化数据的数量
+    bitWidth: 量化数据的位宽
+*/ 
 void TfNToFloat(float *out,
                 uint8_t *in,
                 const unsigned char stepEquivalentTo0,
@@ -163,14 +172,19 @@ void TfNToFloat(float *out,
                 size_t numElement,
                 int bitWidth)
 {
+   // 函数定义了一个循环, 从 0 到 numElement - 1, 依次遍历量化数据
    for (size_t i = 0; i < numElement; ++i) {
+       // 如果 bitWidth 等于8
        if (8 == bitWidth) {
+           // 则函数将 in[i] -> double, 并且将其减 stepEquivalentTo0, 然后乘以 quantizedStepSize, 最后将结果赋值给 out[i]
            double quantizedValue = static_cast <double> (in[i]);
            double stepEqTo0 = static_cast <double> (stepEquivalentTo0);
            out[i] = static_cast <double> ((quantizedValue - stepEqTo0) * quantizedStepSize);
        }
+       // 如果 bitWidth 等于16
        else if (16 == bitWidth) {
            uint16_t *temp = (uint16_t *)in;
+           // 则将 (uint16_t *)in 转换为 double 型 
            double quantizedValue = static_cast <double> (temp[i]);
            double stepEqTo0 = static_cast <double> (stepEquivalentTo0);
            out[i] = static_cast <double> ((quantizedValue - stepEqTo0) * quantizedStepSize);
@@ -178,6 +192,15 @@ void TfNToFloat(float *out,
    }
 }
 
+/*
+    out: 指向输出数组的指针，将存储量化后的值
+    stepEquivalentTo0: 指向一个无符号字符变量的指针，将存储最接近 0 的步长值
+    quantizedStepSize: 指向一个浮点变量的指针，将存储量化步长值
+    staticQuantization: 一个布尔标志，指示量化是静态的还是动态的
+    in: 指向输入数组的指针，包含浮点值
+    numElement: 输入数组的元素个数
+    bitWidth: 输出数组的位宽
+*/
 bool FloatToTfN(uint8_t* out,
                 unsigned char& stepEquivalentTo0,
                 float& quantizedStepSize,
@@ -186,12 +209,16 @@ bool FloatToTfN(uint8_t* out,
                 size_t numElement,
                 int bitWidth)
 {
+   // 储编码最小值、编码最大值、编码范围和真位宽最大值
    double encodingMin;
    double encodingMax;
    double encodingRange;
    double trueBitWidthMax = pow(2, bitWidth) -1;
 
+   // 检查 staticQuantization 标志。如果标志未被设置，则代码进入 if 语句
    if (!staticQuantization) {
+      // 找到输入数组中的最小值和最大值。std::numeric_limits <float>::max() 函数返回一个 float 可以表示的最大值，
+      // std::numeric_limits <float>::min() 函数返回一个 float 可以表示的最小值
       float trueMin = std::numeric_limits <float>::max();
       float trueMax = std::numeric_limits <float>::min();
 
@@ -237,15 +264,20 @@ bool FloatToTfN(uint8_t* out,
    }
    else
    {
+      // 计算 encodingMin 和 encodingMax
       if (bitWidth == 8) {
-         encodingMin = (0 - static_cast <uint8_t> (stepEquivalentTo0)) * quantizedStepSize;
+        // 如果位宽为 8 位，则 encodingMin 将被设置为 (0 - 最接近 0 的步长值) 乘以量化步长
+        encodingMin = (0 - static_cast <uint8_t> (stepEquivalentTo0)) * quantizedStepSize;
       } else if (bitWidth == 16) {
-         encodingMin = (0 - static_cast <uint16_t> (stepEquivalentTo0)) * quantizedStepSize;
+        // 位宽为 16 位，则 encodingMin 将被设置为 (0 - 最接近 0 的步长值的 uint16_t 表示) 乘以量化步长
+        encodingMin = (0 - static_cast <uint16_t> (stepEquivalentTo0)) * quantizedStepSize;
       } else {
          std::cerr << "Quantization bitWidth is invalid " << std::endl;
          return false;
       }
+      // encodingMax 将被设置为 trueBitWidthMax 减去最接近 0 的步长值，并乘以量化步长
       encodingMax = (trueBitWidthMax - stepEquivalentTo0) * quantizedStepSize;
+      // encodingRange 将被设置为 encodingMax 减去 encodingMin
       encodingRange = encodingMax - encodingMin;
    }
 
@@ -338,9 +370,10 @@ bool loadByteDataFileBatchedTf8(const std::string& inputFile, std::vector<uint8_
 
 bool SaveITensorBatched(const std::string& path, const zdl::DlSystem::ITensor* tensor, size_t batchIndex, size_t batchChunk)
 {
+   // 如果 batchChunk 为0, 则将其设置为 tensor 的大小
    if(batchChunk == 0)
       batchChunk = tensor->getSize();
-   // Create the directory path if it does not exist
+   // 创建输出文件夹，如果它不存在
    auto idx = path.find_last_of('/');
    if (idx != std::string::npos)
    {
@@ -353,6 +386,7 @@ bool SaveITensorBatched(const std::string& path, const zdl::DlSystem::ITensor* t
       }
    }
 
+   // 打开输出文件进行写入。
    std::ofstream os(path, std::ofstream::binary);
    if (!os)
    {
@@ -360,6 +394,7 @@ bool SaveITensorBatched(const std::string& path, const zdl::DlSystem::ITensor* t
       return false;
    }
 
+   // 将 `tensor` 中从 `batchIndex * batchChunk` 到 `(batchIndex+1) * batchChunk` 的元素写入文件。
    for ( auto it = tensor->cbegin() + batchIndex * batchChunk; it != tensor->cbegin() + (batchIndex+1) * batchChunk; ++it )
    {
       float f = *it;
@@ -374,6 +409,7 @@ bool SaveITensorBatched(const std::string& path, const zdl::DlSystem::ITensor* t
 
 bool SaveUserBufferBatched(const std::string& path, const std::vector<uint8_t>& buffer, size_t batchIndex, size_t batchChunk)
 {
+   // 如果 batchChunk 参数是 0, 则将其设置为 buffer 的大小, 意味着整个 buffer 做一个批次处理
    if(batchChunk == 0)
       batchChunk = buffer.size();
    // Create the directory path if it does not exist
@@ -381,6 +417,7 @@ bool SaveUserBufferBatched(const std::string& path, const std::vector<uint8_t>& 
    if (idx != std::string::npos)
    {
       std::string dir = path.substr(0, idx);
+      // 确认批处理的目录是否存在, 如果不存在, 则创建
       if (!EnsureDirectory(dir))
       {
          std::cerr << "Failed to create output directory: " << dir << ": "
@@ -389,6 +426,7 @@ bool SaveUserBufferBatched(const std::string& path, const std::vector<uint8_t>& 
       }
    }
 
+   // 打开 path 文件，以二进制方式写入
    std::ofstream os(path, std::ofstream::binary);
    if (!os)
    {
@@ -396,6 +434,7 @@ bool SaveUserBufferBatched(const std::string& path, const std::vector<uint8_t>& 
       return false;
    }
 
+   // 循环遍历 buffer 的当前批次。对于每个元素，将其写入文件
    for ( auto it = buffer.begin() + batchIndex * batchChunk; it != buffer.begin() + (batchIndex+1) * batchChunk; ++it )
    {
       uint8_t u = *it;
@@ -410,6 +449,7 @@ bool SaveUserBufferBatched(const std::string& path, const std::vector<uint8_t>& 
 
 void setResizableDim(size_t resizableDim)
 {
+    // 将 resizableDim 的值赋值给 resizable_dim 变量
     resizable_dim = resizableDim;
 }
 
